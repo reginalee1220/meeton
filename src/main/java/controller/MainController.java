b0412b3d20903ce319e69a2947f474d2ac7f0405
nahyeon <iohyeonsz@gmail.com>
2021-05-28 06:07:23 +0900

    mybatis 버전으로 생긴 오류 해결 ... 서로 다른 파일들 상호작용 버전 < pom.xml >

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -1,25 +1,54 @@
 package controller;
 
+import model.Channel;
+import model.Favorite;
+import model.Video;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import service.MainService;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Controller
 public class MainController {
     @Autowired
     private MainService main;
 
+    /* Main 목록 */
     @RequestMapping("/main.do")
-    public String main(Model model){
+    public String main(Model model, HttpServletRequest request, HttpSession session) {
+//        //My 구독,즐겨찾기 채널
+//        List<Favorite> favoriteList = new ArrayList<Favorite>();
+//        /* HttpSession session = request.getSession(); */
+//        String userid = (String)session.getAttribute("userid");
+//
+//        favoriteList = main.getFavoriteList();
+//        // Video 목록
+//        List<Video> videoList = new ArrayList<Video>();
+//        videoList = main.getVideoList();
+//        model.addAttribute("videoList",videoList);
+
+//        //channel 목록
+//        List<Channel> channelList = new ArrayList<Channel>();
+//        channelList = main.getChannelList();
+//        model.addAttribute("channelList",channelList);
         int count = main.getcount();
 
-        model.addAttribute("count", count);
         return "main";
     }
+
+
+
+
+
+
 
     @RequestMapping("call")
     public String call(Model model){
