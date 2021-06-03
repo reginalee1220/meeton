de2d38b7a8bfe85d394cb4e3f42eef54ab7151bd
ReginaLee <reginalee1220@gmail.com>
2021-06-03 12:52:15 +0900

    kakao api 작업중

--- src/main/java/controller/PurchasedController.java
+++ src/main/java/controller/PurchasedController.java
@@ -1,10 +1,74 @@
 package controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import service.PurchasedService;
+
+
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 
 @Controller
 public class PurchasedController {
+    @Autowired
+    private PurchasedService purchase;
+
+    // 구독버튼 누르면 popup 창으로 이동
+    @RequestMapping("/purchase.do")
+    public String purchase(){
+       return "purchasingPopUp";
+    }
+
+    // 카카오페이 버튼 누르면 카카오페이 연결
+    @RequestMapping("/kakaoPay.do")
+    public String kakaopay() {
+        System.out.println("kakaoPay");
+        try{
+            URL address = new URL("https://kapi.kakao.com/v1/payment/ready");
+            HttpURLConnection connect = (HttpURLConnection) address.openConnection();
+            connect.setRequestMethod("POST");
+            connect.setRequestProperty("Authorization","KakaoAK 47c732bc2e4371c8666855da3c56c538");
+            connect.setRequestProperty("Content-type","application/x-www-form-urlencoded;charset=utf-8");
+            connect.setDoOutput(true);
+
+            String param = "cid=TC0ONETIME&partner_order_id=partner_order_id&partner_user_id=partner_user_id&item_name=구독&quantity=1&total_amount=100&vat_amount=10&tax_free_amount=0&approval_url=http://localhost/success.do&fail_url=http://localhost/fail.do&cancel_url=http://localhost/cancel.do";
+            OutputStream giver = connect.getOutputStream();
+            DataOutputStream dataGiver = new DataOutputStream(giver);
+            dataGiver.writeBytes(param);
+            dataGiver.close();
+
+            int resultCode = connect.getResponseCode();
+
+            InputStream recipient;
+            if(resultCode == 200){ // 연결성공
+                System.out.println("connect success");
+                recipient = connect.getInputStream();
+            }else {                // 연결실패
+                recipient = connect.getErrorStream();
+            }
+
+            InputStreamReader reader = new InputStreamReader(recipient);
+            BufferedReader change = new BufferedReader(reader);
+            System.out.println("change");
+            return change.readLine();
+
+        }catch(MalformedURLException e){
+            e.printStackTrace();
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "null";
+    }
+
+
+
+
+    // 결제 리스트 불러오기
     @RequestMapping("/purchasedList.do")
     public String purchasedList(){
 
--- src/main/webapp/WEB-INF/views/purchasingPopUp.jsp
+++ src/main/webapp/WEB-INF/views/purchasingPopUp.jsp
@@ -1,16 +1,50 @@
-<%--
-  Created by IntelliJ IDEA.
-  User: nahyeon
-  Date: 2021/05/15
-  Time: 4:28 오후
-  To change this template use File | Settings | File Templates.
---%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<!DOCTYPE html>
 <html>
 <head>
-    <title>Title</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>meeton</title>
+    <script src="http://code.jquery.com/jquery-latest.js"></script>
+    <style>
+        input[type=button]{
+            width: 90px;
+            height: 30px;
+            font-size: 13px;
+            color: #a0a0a0;
+            background-color: #f8fafc;
+            border: 1px solid #d9dddf;
+            border-radius: 2px;
+            cursor: pointer;
+            line-height: 30px;
+            vertical-align: center;
+        }
+        input[type=button]:hover{
+            color: #4BA9E1;
+            border: 1px solid #4BA9E1;
+        }
+    </style>
+    <script>
+        $(function(){
+            $('#apibtn').click(function (){
+                $.ajax({
+                    url:'kakaoPay.do',
+                    dataType:'json',
+                    success:function (data){
+                        var box = data.next_redirect_pc_url;
+                        window.open(box);
+                    },
+                    error:function (error){
+                        alert("결제할 수 없습니다.");
+                    }
+                });
+            });
+        })
+
+    </script>
 </head>
 <body>
-
+    <input type="button" value="KakaoPay" id="apibtn" >
 </body>
 </html>
