eac8a71ec6e521a23959d5aebed6936dd67e88de
nahyeon <iohyeonsz@gmail.com>
2021-06-09 13:53:35 +0900

    MainDAO 없어서 추가 commit

---
+++ src/main/java/dao/MainDAO.java
@@ -0,0 +1,15 @@
+package dao;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MainDAO {
+    @Autowired
+    private SqlSessionTemplate sst;
+
+    public int getcount(){
+        return sst.selectOne("main.getcount");
+    }
+}
